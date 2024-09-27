65d2dd55fca7cee5ccf38e42b8e6eb8af2b5a4b5
dell <xxxezv@gmail.com>
2024-09-27 22:54:05 +0300

    hello4

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(bodyParser.json());
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
 // app.use("/", routes);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);

99c0302d6d2f82332d236ab2aea088ae9343f7b3
dell <xxxezv@gmail.com>
2024-09-27 22:44:36 +0300

    hello2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const routes = require("./routes/routes");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(bodyParser.json());
 app.get("/", (req, res) => {

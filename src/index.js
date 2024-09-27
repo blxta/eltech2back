deab99008bc3e83714256c0d9434a3b729dfa7dc
dell <xxxezv@gmail.com>
2024-09-28 00:17:37 +0300

    s

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://eltech2.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("Hello World");

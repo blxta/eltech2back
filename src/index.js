b33f3716d84357d9ed5aeee24072cb13f6f03b89
dell <xxxezv@gmail.com>
2024-09-27 23:39:59 +0300

    m

--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
+//
+
 const express = require("express");
-const bodyParser = require("body-parser");
-const routes = require("./routes/routes");
-const cors = require("cors");
-
 const app = express();
-const PORT = process.env.PORT || 8000;
-app.use(cors());
-app.use(bodyParser.json());
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
-// app.use("/", routes);
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(PORT, () => {
-  console.log(`API is listening on port ${PORT}`);
-});
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
